4bbf4f3921e5da023ee98063d4a4515b16b86563
Administrator <Administrator@WIN-KD37TCD67TL>
2016-06-16 21:47:58 +0800

    all DONE!

--- 矩阵/源.cpp
+++ 矩阵/源.cpp
@@ -1,26 +1,101 @@
+/*Description
+给你一个 N 行 M 列的矩阵，其中第 i 行，第 j 列的元素 Aij = 2 * i * j + 3 * i + 3 * j + 3。求第 k 小的数字。
+
+Input
+一行 3 个整数 N M k。
+
+0 < N, M < 1e5
+0 < k < N * M + 1
+
+Output
+输出第 k 小的数字。*/
+
 //第 i 行，第 j 列的元素 Aij = 2 * i * j + 3 * i + 3 * j + 3。求第 k 小的数字。
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
+
+#include <iostream> 
+#include <algorithm> 
 using namespace std;
-bool myfunction(int i, int j) 
-{ 
-	return (i<j); 
-}//升序
+
+typedef long long vivia;
+
+vivia N, M;
+vivia Cee(vivia mid);
+vivia Number(vivia i, vivia x);
+vivia Function(vivia i, vivia j);
+vivia maxtmp = 0;
+vivia d = 0, d1 = 0;
+vivia cxz;
+vivia k;
+
+vivia Function(vivia i, vivia j) {
+	return 2ll * i*j + 3ll * i + 3ll * j + 3ll;
+}
+
+vivia Cee(vivia mid) {
+	d = 0;
+	vivia count = 0;
+	cxz = M;
+	for (int i = 1; i <= N; i++) {
+		if (Function(i, M)<mid) { count += M; continue; }
+		if (Function(i, 1)>mid) { break; }
+		count += Number(i, mid);
+		if (count >= k) return count;
+	}
+	return count;
+}
+
+vivia Number(vivia i, vivia x) {
+	if (Function(i, M)<x) return M;
+	if (Function(i, 1)>x) return 0;
+	vivia r1 = 0;
+	for (int j = cxz; j >= 1; j--) {
+		if (Function(i, j) == x) { d++; d1 = x; }
+		if (Function(i, j)<x) { cxz = j; return j; }
+	}
+	return r1;
+}
 int main()
 {
-	int N, M , k;
-	vector<int> v;
-
-	cin >> N >> M;
-	cin >> k;
-	for(int i=0;i<N;i++)
-		for (int j = 0;j < M;j++)
-		{
-			v.push_back(2 * (i+1) * (j+1) + 3 * (i + 1) + 3 * (j + 1) + 3);
+	
+	while ( cin>>N >>M>>k)
+	{
+		
+		cout << "";
+		vivia lo = 11ll, hi = Function(N, M);
+		cout << "";
+		while (lo <hi) {
+			cout << "";
+			vivia mid = (lo + hi + 1) / 2;
+			cout << "";
+			vivia xiao = Cee(mid);
+			cout << "";
+			if (xiao + d >= k&&xiao<k) 
+			{ 
+				hi = mid; 
+				cout << "";
+				break; 
+			}
+			if (xiao <= k)
+			{
+				cout << "";
+				if (xiao == k) 
+					hi = mid - 1;
+				else 
+					lo = mid;
+			}
+			else
+			{
+				hi = mid;
+				cout << "";
+				
+				
+			}
 		}
-	sort(v.begin(), v.end(),myfunction);
-	cout << v[k - 1] << endl;
-	return 0;
+		printf("%lld\n", hi);
+		
+		
+		cout << "";
+	}
+
 }
